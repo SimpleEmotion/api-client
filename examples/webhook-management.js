3a579d2f131b9a2646effe9629987c000de6f716
Akash Krishnan <ak@aakay.net>
2017-04-13 18:48:32 -0700

    added colors to webhook management example

--- examples/webhook-management.js
+++ examples/webhook-management.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require( 'colors' );
+
 // INFO: Obtain credentials from https://console.simpleemotion.com
 // INFO: Obtain user id from https://console.simpleemotion.com/api/user
 
@@ -15,13 +17,13 @@ const Config = {
   service: 'webhooks'
 };
 
-const URL = 'https://se-ws-zach.dev.simpleemotion.com:9000';
+const URL = 'https://se-ws-akash.dev.simpleemotion.com';
 const SECRET = 'IM SUPER SECRET';
 const EVENT = {
   type: 'storage.audio.added'
 };
 
-const API = require( '..' )( null, null, { scope: 'webhook', host: 'https://api2.simpleemotion.com' } );
+const API = require( '..' )( null, null, { scope: 'webhook', host: 'https://api.simpleemotion.com' } );
 API.credentials = Config.credentials;
 
 if ( require.main === module ) {
@@ -29,88 +31,105 @@ if ( require.main === module ) {
 }
 
 function main() {
-  console.log();
+
   console.log( '[!] Adding webhook.' );
 
-  API.webhook.add( {
-    webhook: {
-      owner: Config.owner,
-      url: URL,
-      secret: SECRET,
-      event: EVENT
-    }
-  }, function ( err, result ) {
-
-    if ( err ) {
-      return console.error( err );
-    }
-
-    var webhook = result.webhook;
-
-    console.log( '[!] Added webhook:', webhook );
-    console.log();
-    console.log( '[!] Listing webhooks for owner ' + Config.owner._id + '.' );
-    console.log( '[!] NOTE: This list includes webhooks added to this owner by all sources.' );
-
-    API.webhook.list( {
+  API.webhook.add(
+    {
       webhook: {
-        owner: Config.owner
+        owner: Config.owner,
+        url: URL,
+        secret: SECRET,
+        event: EVENT
       }
-    }, function ( err, result ) {
+    },
+    function ( err, result ) {
+
       if ( err ) {
         return console.error( err );
       }
 
-      console.log( '[!] Webhooks:' );
+      const webhook = result.webhook;
 
-      result.webhooks.forEach( function ( w ) {
-        console.log( '\t_id:', w._id, '\tevent.type:', w.event.type );
-      } );
+      console.log( JSON.stringify( result, null, 2 ).bgBlack );
 
       console.log();
-      console.log( '[!] Removing the webhook we added with _id: ' + webhook._id + '.' );
+      console.log( `[!] Listing webhooks for owner ${Config.owner._id}.` );
+      console.log( '[!] NOTE: This list includes webhooks added to this owner by all sources.' );
 
-      API.webhook.remove( {
-        webhook: { _id: webhook._id }
-      }, function ( err ) {
-        if ( err ) {
-          return console.error( err );
-        }
-
-        console.log( '[!] Removed webhook.' );
-
-        console.log();
-        console.log( '[!] Listing webhooks for owner ' + Config.owner._id + '.' );
-        console.log( '[!] NOTE: This list includes webhooks added to this owner by all sources.' );
-
-        API.webhook.list( {
+      API.webhook.list(
+        {
           webhook: {
             owner: Config.owner
           }
-        }, function ( err, result ) {
+        },
+        function ( err, result ) {
+
           if ( err ) {
             return console.error( err );
           }
 
-          console.log( '[!] Webhooks:' );
+          if ( !result.total ) {
+            console.log( 'No webhooks found.'.bgBlack );
+          }
 
-          result.webhooks.forEach( function ( w ) {
-            console.log( '\t_id:', w._id, '\tevent.type:', w.event.type );
+          result.webhooks.forEach( function ( w, i ) {
+            console.log( `[${i}]\t_id: ${w._id}\tevent.type: ${w.event.type}`.bgBlack );
           } );
 
-          console.log( '[!] NOTE: Webhook was removed.' );
           console.log();
-          console.log();
-          console.log( '[ALL DONE!!!]' );
-          console.log();
-          console.log();
+          console.log( `[!] Removing the webhook we added with _id: ${webhook._id}.` );
 
-        } );
+          API.webhook.remove(
+            {
+              webhook: webhook
+            },
+            function ( err, result ) {
 
-      } );
+              if ( err ) {
+                return console.error( err );
+              }
 
-    } );
+              console.log( JSON.stringify( result, null, 2 ).bgBlack );
 
-  } );
+              console.log();
+              console.log( '[!] Listing webhooks for owner ' + Config.owner._id + '.' );
+              console.log( '[!] NOTE: This list includes webhooks added to this owner by all sources.' );
+
+              API.webhook.list(
+                {
+                  webhook: {
+                    owner: Config.owner
+                  }
+                },
+                function ( err, result ) {
+
+                  if ( err ) {
+                    return console.error( err );
+                  }
+
+                  if ( !result.total ) {
+                    console.log( 'No webhooks found.'.bgBlack );
+                  }
+
+                  result.webhooks.forEach( function ( w, i ) {
+                    console.log( `[${i}]\t_id: ${w._id}\tevent.type: ${w.event.type}`.bgBlack );
+                  } );
+
+                  console.log();
+                  console.log( '[ALL DONE!!!]' );
+                  console.log();
+
+                }
+              );
+
+            }
+          );
+
+        }
+      );
+
+    }
+  );
 
 }
